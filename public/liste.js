6f11424aa0adc5fea3d07eecb11936d05e32e276
Your Name <you@example.com>
2021-11-02 21:25:59 +0100

    J'ai fais tous ce que j'ai pu

--- public/liste.js
+++ public/liste.js
@@ -1,109 +1,185 @@
-document.addEventListener('DOMContentLoaded', async (req, res) => {
+document.addEventListener("DOMContentLoaded", async (req, res) => {
 
-    const DateTime = luxon.DateTime;
-    let session;
-    fetch('/session').then((userdata) => {
+  const DateTime = luxon.DateTime;
+  let session;
+  fetch("/session")
+    .then((userdata) => {
+      return userdata.json();
+    })
+    .then((dataUser) => {
+      if (dataUser.userid == undefined || dataUser.userid == null) {
+        window.location.replace("/");
+      } else {
+        session = dataUser;
 
-        return userdata.json()
+        if (window.location.pathname.startsWith("/listePartie")) {
+          const interval = 5;
+          const validTime = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
 
-    }).then((dataUser) => {
+          let next_time = DateTime.now();
+          let check = validTime.find((t) => t >= next_time.second);
+          let cloak_time = check ? check : validTime[0];
 
-        if (dataUser.userid == undefined || dataUser.userid == null) {
-            window.location.replace("/")
-        } else {
-            session = dataUser;
+          let params = {};
+          if (cloak_time == 0) {
+            params.minutes = next_time.minute + 1;
+            params.seconds = 0;
+          } else {
+            params.seconds = cloak_time;
+          }
+          let new_time = DateTime.now().set(params);
 
-            if (window.location.pathname.startsWith("/listePartie")) {
-                const interval = 5;
-                const validTime = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+          refreshParties();
 
-                let next_time = DateTime.now();
-                let check = validTime.find(t => t >= next_time.second);
-                let cloak_time = (check) ? check : validTime[0];
-
-                let params = {};
-                if (cloak_time == 0) {
-                    params.minutes = next_time.minute + 1;
-                    params.seconds = 0;
-                } else {
-                    params.seconds = cloak_time;
-                }
-                let new_time = DateTime.now().set(params);
-
-                refreshParties();
-
-                setTimeout(async () => {
-                    setInterval(async () => {
-
-                        console.log("refresh good");
-                        await refreshParties();
-                    }, interval * 1000);
-
-
-
-                }, new_time.diff(DateTime.now()).toObject().milliseconds)
-
-            }
-
-            async function refreshParties() {
-                const liste = document.querySelector(".listePartie");
-                let actualItems = [];
-
-                for (let index = 0; index < liste.children.length; index++) {
-                    const element = liste.children[index];
-                    actualItems.push(element.id);
-                }
-
-                await fetch("/recupPartie").then(reponse => reponse.json())
-                    .then(async data => {
-                        data.forEach(async game => {
-
-                            await actualItems.splice(actualItems.indexOf(game._id), 1);
-
-                            if (document.getElementById(game._id)) {
-                                let item = document.getElementById(game._id);
-                                if (game.createurPartie == session.userid) {
-                                    item.innerHTML = " Nom de la partie: " + game.nomPartie + "<br> Joueur 1: " + game.J1.nomJ1 + "<br> Joueur 2 : " + game.J2.nomJ2 + "<br> Etat : " + game.statue + "<br><form action='/deletePartie' method='get'><button class='w3-button' type='submit'" + game._id + "'>Supprimer</button></form> "
-                                        + "<br> <form action='/partieEncour/" + game._id + "' method='get'><button class='w3-button' type='submit'" + game._id + "'>Rejoindre</button></form>";
-                                } else {
-                                    item.innerHTML = " Nom de la partie: " + game.nomPartie + "<br> Joueur 1: " + game.J1.nomJ1 + "<br> Joueur 2 : " + game.J2.nomJ2 + "<br> Etat : " + game.statue
-                                        + "<br> <form action='/partieEncour/" + game._id + "' method='get'><button class='w3-button' type='submit'" + game._id + "'>Rejoindre</button></form>";
-                                }
-
-
-
-                            }
-                            else {
-                                let li = document.createElement("li");
-                                li.id = game._id
-                                if (game.createurPartie == session.userid) {
-                                    li.innerHTML = " Nom de la partie: " + game.nomPartie + "<br> Joueur 1: " + game.J1.nomJ1 + "<br> Joueur 2 : " + game.J2.nomJ2 + "<br> Etat : " + game.statue + "<br><form action='/deletePartie' method='get'><button class='w3-button' type='submit'" + game._id + "'>Supprimer</button></form> "
-                                        + "<br> <form action='/partieEncour/" + game._id + "' method='get'><button class='w3-button' type='submit'" + game._id + "'>Rejoindre</button></form>";
-                                    liste.appendChild(li);
-                                } else {
-                                    li.innerHTML = " Nom de la partie: " + game.nomPartie + "<br> Joueur 1: " + game.J1.nomJ1 + "<br> Joueur 2 : " + game.J2.nomJ2 + "<br> Etat : " + game.statue
-                                        + "<br> <form action='/partieEncour/" + game._id + "' method='get'><button class='w3-button' type='submit'" + game._id + "'>Rejoindre</button></form>";
-                                    liste.appendChild(li);
-                                }
-
-
-
-                            }
-
-                        });
-                    })
-                await actualItems.forEach(actualItem => {
-                    let obj = document.getElementById(actualItem);
-                    obj.parentNode.removeChild(obj);
-                })
-            }
-
+          comboAD();
+          
+          setTimeout(async () => {
+            setInterval(async () => {
+              console.log("refresh good");
+              await refreshParties();
+              await comboAD();
+            }, interval * 1000);
+          }, new_time.diff(DateTime.now()).toObject().milliseconds);
         }
 
-    })
+        async function refreshParties() {
+          const liste = document.querySelector(".listePartie");
+          let actualItems = [];
 
+          for (let index = 0; index < liste.children.length; index++) {
+            const element = liste.children[index];
+            actualItems.push(element.id);
+          }
 
+          await fetch("/recupPartie")
+            .then((reponse) => reponse.json())
+            .then(async (data) => {
+              data.forEach(async (game) => {
+                await actualItems.splice(actualItems.indexOf(game._id), 1);
 
+                if (document.getElementById(game._id)) {
+                  let item = document.getElementById(game._id);
+                  if (game.createurPartie == session.userid) {
+                    item.innerHTML =
+                      " Nom de la partie: " +
+                      game.nomPartie +
+                      "<br> Joueur 1: " +
+                      game.J1.nomJ1 +
+                      "<br> Joueur 2 : " +
+                      game.J2.nomJ2 +
+                      "<br> Etat : " +
+                      game.statue +
+                      "<br><form action='/deletePartie/" +
+                      game._id +
+                      "' method='get'><button class='w3-button' type='submit'>Supprimer</button></form> " +
+                      "<br> <form action='/partieEncour/" +
+                      game._id +
+                      "' method='get'><button class='w3-button' type='submit'" +
+                      game._id +
+                      "'>Rejoindre</button></form>";
+                  } else {
+                    item.innerHTML =
+                      " Nom de la partie: " +
+                      game.nomPartie +
+                      "<br> Joueur 1: " +
+                      game.J1.nomJ1 +
+                      "<br> Joueur 2 : " +
+                      game.J2.nomJ2 +
+                      "<br> Etat : " +
+                      game.statue +
+                      "<br> <form action='/partieEncour/" +
+                      game._id +
+                      "' method='get'><button class='w3-button' type='submit'" +
+                      game._id +
+                      "'>Rejoindre</button></form>";
+                  }
+                } else {
+                  let li = document.createElement("li");
+                  li.id = game._id;
+                  if (game.createurPartie == session.userid) {
+                    li.innerHTML =
+                      " Nom de la partie: " +
+                      game.nomPartie +
+                      "<br> Joueur 1: " +
+                      game.J1.nomJ1 +
+                      "<br> Joueur 2 : " +
+                      game.J2.nomJ2 +
+                      "<br> Etat : " +
+                      game.statue +
+                      "<br><form action='/deletePartie/" +
+                      game._id +
+                      "' method='get'><button class='w3-button' type='submit'>Supprimer</button></form> " +
+                      "<br> <form action='/partieEncour/" +
+                      game._id +
+                      "' method='get'><button class='w3-button' type='submit'" +
+                      game._id +
+                      "'>Rejoindre</button></form>";
+                    liste.appendChild(li);
+                  } else {
+                    li.innerHTML =
+                      " Nom de la partie: " +
+                      game.nomPartie +
+                      "<br> Joueur 1: " +
+                      game.J1.nomJ1 +
+                      "<br> Joueur 2 : " +
+                      game.J2.nomJ2 +
+                      "<br> Etat : " +
+                      game.statue +
+                      "<br> <form action='/partieEncour/" +
+                      game._id +
+                      "' method='get'><button class='w3-button' type='submit'" +
+                      game._id +
+                      "'>Rejoindre</button></form>";
+                    liste.appendChild(li);
+                  }
+                }
+              });
+            });
+          await actualItems.forEach((actualItem) => {
+            let obj = document.getElementById(actualItem);
+            obj.parentNode.removeChild(obj);
+          });
+        }
 
+        async function comboAD() {
+            const liste = document.querySelector(".comboAD");
+            let actualItems = [];
 
-})
+            for (let index = 0; index < liste.children.length; index++) {
+                const element = liste.children[index];
+                actualItems.push(element.id);
+            }
+
+            await fetch("/comboAD").then(reponse => reponse.json())
+                .then(async data => {
+                    data.forEach(async users => {
+                        console.log(users);
+
+                        await actualItems.splice(actualItems.indexOf(users._id), 1);
+
+                        if (document.getElementById(users._id)) {
+                            let item = document.getElementById(users._id);
+
+                            item.innerHTML = users.pseudo;
+                            liste.appendChild(item);
+
+                        }
+                        else {
+                            let select = document.createElement("option");
+                            select.id = users._id
+
+                            select.innerHTML = users.pseudo;
+                            liste.appendChild(select);
+
+                        }
+
+                    });
+                })
+            await actualItems.forEach(actualItem => {      
+                let obj = document.getElementById(actualItem);
+                obj.parentNode.removeChild(obj);
+            })
+        }
+      }
+    });
+});
--- serveur.js
+++ serveur.js
@@ -1,211 +1,230 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
-const sessions = require('express-session')
-const { stringify } = require('querystring')
-const mongoDBsession = require('connect-mongodb-session')(sessions)
-const fs = require('fs');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const sessions = require("express-session");
+const { stringify } = require("querystring");
+const mongoDBsession = require("connect-mongodb-session")(sessions);
+const fs = require("fs");
 
-const saleMdp = 10
+const saleMdp = 10;
 app.use(express.json());
-app.use(express.static("./public"))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.static("./public"));
+app.use(express.urlencoded({ extended: true }));
 
-app.set('views', "./public");
-app.set('view engine', 'ejs');
+app.set("views", "./public");
+app.set("view engine", "ejs");
 
 // let methodOverride = require('method-override')
 // const { boolean } = require('webidl-conversions')
 
-
-mongoose.connect("mongodb://localhost:27017/ProjetJS", { useNewUrlParser: true }).then(() => //connection en localhost
-    console.log("Connected to Mongo…")).catch((error) =>                                //affiche message
-        console.log(error.message))
-
+mongoose
+  .connect("mongodb://localhost:27017/ProjetJS", { useNewUrlParser: true })
+  .then(() =>
+    //connection en localhost
+    console.log("Connected to Mongo…")
+  )
+  .catch(
+    (
+      error //affiche message
+    ) => console.log(error.message)
+  );
 
 const usersSchema = mongoose.Schema({
-    pseudo: String,
-    mdp: String
+  pseudo: String,
+  mdp: String,
 });
 
 const sessionSchema = new mongoose.Schema({
-    session: {
-        userid: String,
-        isAuth: Boolean
-    }
-})
-
+  session: {
+    userid: String,
+    isAuth: Boolean,
+  },
+});
 
 const partiesSchema = mongoose.Schema({
-    nomPartie: String,
-    lot: { type: Number, required: true, default: 6 },
-    J1: {
-        nomJ1: { type: String,  required: true },
-        jetons: { type: Number, default: 100 },
-        isConnected: { type: Boolean, required: true, default: false },
-    },
-    J2: {
-        nomJ2: { type: String,  required: true },
-        jetons: { type: Number, default: 100 },
-        isConnected: { type: Boolean, required: true, default: false },
-    },
-    round: { type: Number, required: true, default: 1 },
-    spectateur: { type: Array },
-    createurPartie: String,
-    statue: { type: String, enum: ['en Attente', 'En cours', 'Terminé'], default: 'en Attente' },
-    date: { type: Date, default: Date.now },
-})
+  nomPartie: String,
+  lot: { type: Number, required: true, default: 6 },
+  J1: {
+    nomJ1: { type: String, required: true },
+    jetons: { type: Number, default: 100 },
+    isConnected: { type: Boolean, required: true, default: false },
+  },
+  J2: {
+    nomJ2: { type: String, required: true },
+    jetons: { type: Number, default: 100 },
+    isConnected: { type: Boolean, required: true, default: false },
+  },
+  round: { type: Number, required: true, default: 1 },
+  spectateur: { type: Array },
+  createurPartie: String,
+  statue: {
+    type: String,
+    enum: ["en Attente", "En cours", "Terminé"],
+    default: "en Attente",
+  },
+  date: { type: Date, default: Date.now },
+});
 
 const store = new mongoDBsession({
-    uri: "mongodb://localhost:27017/ProjetJS",
-    collection: 'sessions'
-})
+  uri: "mongodb://localhost:27017/ProjetJS",
+  collection: "sessions",
+});
 
 const Users = mongoose.model("Users", usersSchema);
-const Session = mongoose.model("Session", sessionSchema)
+const Session = mongoose.model("Session", sessionSchema);
 const Parties = mongoose.model("Parties", partiesSchema);
 
-app.use(sessions({
+app.use(
+  sessions({
     secret: "mon petit sercret",
     cookie: { maxAge: 1000 * 60 * 60 * 24 },
     saveUninitialized: false,
     resave: false,
-    store: store
-}));
+    store: store,
+  })
+);
 
 async function createUsers(pseudo, mdp) {
-    bcrypt.genSalt(saleMdp, async function (err, salt) {
-        bcrypt.hash(mdp, salt, async function (err, hash) {
-            const mdhHash = hash
-            bcrypt.compare(mdp, hash, async function (err, result) {
-                if (result) {
-                    mdp = mdhHash
-                    const users = new Users({
-                        pseudo: pseudo,
-                        mdp: mdp,
-                    });
-                    const result = await users.save();
-                    console.log(result);
-                }
-            })
-        })
-    })
+  bcrypt.genSalt(saleMdp, async function (err, salt) {
+    bcrypt.hash(mdp, salt, async function (err, hash) {
+      const mdhHash = hash;
+      bcrypt.compare(mdp, hash, async function (err, result) {
+        if (result) {
+          mdp = mdhHash;
+          const users = new Users({
+            pseudo: pseudo,
+            mdp: mdp,
+          });
+          const result = await users.save();
+          console.log(result);
+        }
+      });
+    });
+  });
 }
 
 app.post("/newParties", (req, res) => {
-    createParties(req.body.nomDesParties, req.body.votrePseudo, req.body.pseudoAd, req.session.userid)
-    res.redirect("/listePartie")
-})
+  createParties(
+    req.body.nomDesParties,
+    req.body.votrePseudo,
+    req.body.pseudoAd,
+    req.session.userid
+  );
+  res.redirect("/listePartie");
+});
 
 async function createParties(nomPartie, j1, j2, createur) {
-    // nomJ1=req.body.votrePseudo
-    // nomJ2=req.body.pseudoAd
-    const parties = new Parties({
-        nomPartie: nomPartie,
-        J1: {
-            nomJ1: j1,
-        },
-        J2: {
-            nomJ2: j2,
-        },
-        createurPartie: createur
-    });
-    const result = await parties.save();
-    console.log(result);
+  // nomJ1=req.body.votrePseudo
+  // nomJ2=req.body.pseudoAd
+  const parties = new Parties({
+    nomPartie: nomPartie,
+    J1: {
+      nomJ1: j1,
+    },
+    J2: {
+      nomJ2: j2,
+    },
+    createurPartie: createur,
+  });
+  const result = await parties.save();
+  console.log(result);
 }
 
-
 app.post("/users", (req, res) => {
-    createUsers(req.body.pseudo, req.body.password)
-    req.session.userid = req.body.pseudo
-    res.redirect("/connexion.html")
-})
-
-// app.post("/usersConnexion", (req, res) => {
-//     Users.findOne({ 'pseudo': req.body.pseudoCo }, function (err, findPseudo) {
-//         if (err) {
-//             console.log(err)
-//         } else if (findPseudo === null || !findPseudo) {
-//             console.log("mauvais pseudo")
-//             res.redirect("/connexion.html")
-//         }
-//         Users.findOne({ 'pseudo': req.body.pseudoCo }, 'mdp', function (err, findPassword) {
-//             if (err) return handleError(err);
-//             bcrypt.compare(req.body.password, findPassword.password, function (err, result) {
-//                 if (result) {
-//                     req.session.userid = req.body.pseudoCo
-//                     res.redirect("/listePartie.html")
-//                 } if (!result) {
-//                     console.log("Mauvais mdp")
-//                     res.redirect("/connexion.html")
-//                 }
-//             });
-//         });
-//     })
-// })
-
-app.get('/',(req,res) => {
-
-    if(req.session.isAuth === true){
-        res.redirect('/listeDesParties.html');
-    }else
-    res.sendFile('Index.html')
+  createUsers(req.body.pseudo, req.body.password);
+  req.session.userid = req.body.pseudo;
+  res.redirect("/connexion.html");
 });
 
-app.post('/usersConnexion',async (req,res) => {
-    await Users.findOne({
+app.get("/", (req, res) => {
+  if (req.session.isAuth === true) {
+    res.redirect("/listeDesParties.html");
+  } else res.sendFile("Index.html");
+});
 
-            pseudo: req.body.pseudoCo
+app.post("/usersConnexion", async (req, res) => {
+  await Users.findOne({
+    pseudo: req.body.pseudoCo,
+  }).then(function (user) {
+    if (!user) return res.redirect("/connexion.html");
+    bcrypt.compare(req.body.passwordCo, user.mdp, function (err, result) {
+      if (!result) return res.redirect("/connexion.html");
 
-    }).then(function (user) {
-        if (!user) return res.redirect('/connexion.html');
-        bcrypt.compare(req.body.passwordCo, user.mdp, function (err, result) {
-            if (!result) return res.redirect('/connexion.html');
+      req.session.isAuth = true;
+      req.session.userid = req.body.pseudoCo;
 
-            req.session.isAuth = true
-            req.session.userid = req.body.pseudoCo
-
-            req.session.user = {
-                id: user._id, 
-                pseudo: user.pseudoCo
-            };
-            // res.send("Hey there, welcome <a href='/logout'>click to logout</a>");
-            res.redirect('/listePartie')
-        });
+      req.session.user = {
+        id: user._id,
+        pseudo: user.pseudoCo,
+      };
+      // res.send("Hey there, welcome <a href='/logout'>click to logout</a>");
+      res.redirect("/listePartie");
     });
-});
- 
-app.get('/logout',(req,res) => {
-    req.session.destroy();
-    res.redirect('/connexion.html');
+  });
 });
 
-app.get('/listePartie',(req,res)=>{
-    if(!req.session.user) return res.redirect('/connexion.html')
-    res.render("listePartie.ejs",{})
-})
+app.get("/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("/connexion.html");
+});
+
+app.get("/listePartie", (req, res) => {
+  if (!req.session.user) return res.redirect("/connexion.html");
+  res.render("listePartie.ejs", {});
+});
 
 app.get("/recupPartie", async (req, res) => {
-    await Parties.find().then((games)=>{
-        res.json(games);
+  await Parties.find().then((games) => {
+    res.json(games);
+  });
+});
+
+app.get("/deletePartie/:id", async (req, res) => {
+  Parties.findOne({ _id: req.params.id }, "_id", async function (err, gamebdd) {
+    await Parties.deleteOne({ _id: req.params.id });
+  });
+});
+
+app.get("/session", (req, res) => {
+  res.json(req.session);
+});
+
+app.get('/partieEncour/:id', (req, res) => {
+    res.render('partieEnCour.ejs');
+})
+
+// app.get("/partieEncour/:id", async (req, res) => {
+//   if (!req.session.user) return res.redirect("/listePartie");
+//   let game = await Parties.findById(req.params.id);
+//   if (!game) return res.redirect("/listePartie");
+
+//   let user1 = await Users.findOne({ "pseudo": game.J1.nomJ1 });
+//   let user2 = await Users.findOne({ "pseudo": game.J2.nomJ2 });
+
+//   let type = null;
+
+//   if (user1.id == req.session.user.id || user2.id == req.session.user.id) {
+//     type = true;
+//   } else {
+//     type = false;
+//   }
+//   console.log(game);
+//   return res.render("partieEnCour.ejs", {
+//     game: game,
+//     user1: user1,
+//     user2: user2,
+//     isplayer: type,
+//   });
+// });
+
+app.get('/comboAD',async(req,res) => {
+    await Users.find()
+    .then((utilisateur)=>{
+        res.json(utilisateur)
     })
 })
-
-app.get("/deletePartie", async (req,res)=>{
-    await Parties.deleteOne().then((suppr)=>{
-        res.redirect("/listePartie");
-    })
-})
-
-app.get('/session', (req, res) => {
-
-    res.json(req.session)
-})
-
-app.get('/partieEncour/:id', (req, res) => res.render('partieEnCour.ejs'));
-
 
 app.listen(3000, () => {
-    console.log("Server started on port...");
+  console.log("Server started on port...");
 });
